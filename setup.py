a8ca6057aeba81ef1a4083fcc343753dcd4cd54d
Andreas Motl <andreas@terkin.org>
2020-04-28 00:30:37 +0200

    Chore: Update README etc.

--- setup.py
+++ setup.py
@@ -11,14 +11,14 @@ setuptools.setup(
     description="A wrapper for the dragino LoRa/GPS HAT",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url = "https://github.com/computenodes/dragino",
+    url = "https://github.com/daq-tools/dragino",
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)'
     ],
-    keywords="LoRaWAN",
+    keywords="Dragino LoRa LoRaWAN",
     platforms='any',
     python_requires='>=3.3, <4',
     install_requires=['configobj', 'spidev', 'rpi-gpio', 'pycrypto'],
