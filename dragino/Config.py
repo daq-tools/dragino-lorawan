ce38a3acca64de3907913da047a785eec3751c2f
Andreas Motl <andreas@terkin.org>
2021-11-01 00:52:35 +0100

    Add alternative way of pure-Python configuration
    This patch is needed for a configuration style where this code is used
    as a library, so the host program will be responsible for supplying all
    configuration settings.
    
    The new configuration helper classes, `LoRaWANAuthentication` and
    `LoRaWANConfig` are used within the Terkin datalogger at [1].
    
    [1] https://github.com/hiveeyes/terkin-datalogger/blob/0.13.0/src/lib/terkin/network/lora.py#L222-L237

--- dragino/Config.py
+++ dragino/Config.py
@@ -1,7 +1,11 @@
 import toml
 import logging
+import binascii
+
+from .Strings import *
 
 DEFAULT_LOGGING_LEVEL=logging.DEBUG
+
 
 class TomlConfig:
     """
@@ -44,3 +48,51 @@ class TomlConfig:
         return self.config
 
 
+class LoRaWANAuthentication:
+    """
+    Represents a pure-Python configuration object for LoRaWAN authentication.
+    """
+
+    def __init__(self, auth_mode=None, devaddr=None, nwskey=None, appskey=None, deveui=None, appeui=None, appkey=None):
+
+        # Authentication mode (ABP or OTAA)
+        self.auth_mode = auth_mode
+
+        # ABP
+        self.devaddr = devaddr
+        self.nwskey = nwskey
+        self.appskey = appskey
+
+        # OTAA
+        self.deveui = deveui
+        self.appeui = appeui
+        self.appkey = appkey
+
+
+class LoRaWANConfig:
+    """
+    Represents a pure-Python configuration object for LoRaWAN.
+    """
+    def __init__(self, auth: LoRaWANAuthentication=None, spreading_factor=7, max_power=0x0F, output_power=0x0E, sync_word=0x34, rx_crc=True, fcount_filename=".lora_fcount"):
+
+        if auth.auth_mode.upper() == "ABP":
+            self.auth = AUTH_ABP
+            self.devaddr = self._convert_address(auth.devaddr)
+            self.nwskey = self._convert_address(auth.nwskey)
+            self.appskey = self._convert_address(auth.appskey)
+
+        elif auth.auth_mode.upper() == "OTAA":
+            self.auth = AUTH_OTAA
+            self.deveui = self._convert_address(auth.deveui)
+            self.appeui = self._convert_address(auth.appeui)
+            self.appkey = self._convert_address(auth.appkey)
+
+        self.spreading_factor = spreading_factor
+        self.max_power = max_power
+        self.output_power = output_power
+        self.sync_word = sync_word
+        self.rx_crc = rx_crc
+        self.fcount_filename = fcount_filename
+
+    def _convert_address(self, address):
+        return list(binascii.unhexlify(address))
